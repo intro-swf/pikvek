5ba11d7e99351eca8ed0357cfa215baedc13559e
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-03-29 12:46:07 +0100

    Update pikvek-main.js

--- pikvek-main.js
+++ pikvek-main.js
@@ -16,6 +16,10 @@ require(['//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min.j
   renderbutton.onclick = function() {
     var svg = parser.parseFromString(svgtext.value, 'image/svg+xml');
     console.log(svg);
+    rendercanvas.width = svg.documentElement.width.baseVal.value;
+    rendercanvas.height = svg.documentElement.height.baseVal.value;
+    var ctx = rendercanvas.getContext('2d');
+    ctx.clearRect(0, 0, rendercanvas.width, rendercanvas.height);
   };
   renderbutton.disabled = false;
   
