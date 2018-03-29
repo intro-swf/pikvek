df3d66a59b5d173c2ac5803ffe17a5ceed57b744
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-03-29 17:13:41 +0100

    Update pikvek-main.js

--- pikvek-main.js
+++ pikvek-main.js
@@ -3,7 +3,13 @@ requirejs.config({
   waitSeconds: 0,
 });
 
-require(['//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min.js'], function() {
+require([
+  '//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min.js!'
+  ,'pikvek/core'
+], function(
+  domReady
+  ,core
+) {
   
   'use strict';
   
@@ -20,5 +26,22 @@ require(['//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min.j
     });
   };
   renderbutton.disabled = false;
+  
+  var ctx = rendercanvas.getContext('2d');
+  ctx.fillStyle = '#fff';
+  core.fill(ctx, [
+    {x:247, y:887},
+    {x:213, y:977},
+    {x:168, y:893},
+    {x:73, y:908},
+    {x:123, y:826},
+    {x:62, y:752},
+    {x:158, y:755},
+    {x:192, y:665},
+    {x:238, y:749},
+    {x:333, y:734},
+    {x:282, y:816},
+    {x:343, y:890},
+  ]);
   
 });
