1457a977363338cc0212931e71d3d9f41688973a
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-03-29 13:09:31 +0100

    Update pikvek-main.js

--- pikvek-main.js
+++ pikvek-main.js
@@ -16,19 +16,17 @@ require(['//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min.j
   renderbutton.onclick = function() {
     var svgdoc = parser.parseFromString(svgtext.value, 'image/svg+xml');
     var svg = svgdoc.documentElement;
-    svg = document.adoptNode(svg);
     console.log(svg);
-    rendercanvas.width = svg.width.baseVal.value;
-    rendercanvas.height = svg.height.baseVal.value;
+    rendercanvas.width = svg.getAttribute('width');
+    rendercanvas.height = svg.getAttribute('height');
     var ctx = rendercanvas.getContext('2d');
     ctx.clearRect(0, 0, rendercanvas.width, rendercanvas.height);
     
     for (var node = svg.firstChild; node; node = node.nextSibling) {
       switch (node.nodeName) {
         case 'path':
-          var style = window.getComputedStyle(node, null);
-          var fill = style.getPropertyValue('fill');
-          var stroke = style.getPropertyValue('stroke');
+          var fill = node.getAttribute('fill') || '#000';
+          var stroke = node.getAttribute('stroke') || 'none';
           console.log(fill, stroke);
           break;
       }
