43aa6edf889e941151456e4b0d07222198382982
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-03-29 12:27:00 +0100

    Update pikvek-main.js

--- pikvek-main.js
+++ pikvek-main.js
@@ -3,10 +3,18 @@ requirejs.config({
   waitSeconds: 0,
 });
 
-require([], function() {
+require(['//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min'], function() {
   
   'use strict';
   
-  console.log('hi');
+  var svgtext = document.getElementById('svgtext');
+  var renderbutton = document.getElementById('renderbutton');
+  var rendercanvas = document.getElementById('rendercanvas');
+  
+  console.log(svgtext, renderbutton, rendercanvas);
+  
+  renderbutton.onclick = function() {
+  };
+  renderbutton.disabled = false;
   
 });
