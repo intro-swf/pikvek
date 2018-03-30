84df9ce26158e44a47c5d1e360e919cb9c2516cd
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-03-30 11:10:51 +0100

    Update pikvek-main.js

--- pikvek-main.js
+++ pikvek-main.js
@@ -5,9 +5,11 @@ requirejs.config({
 
 require([
   '//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min.js!'
+  ,'pathData'
   ,'pikvek/core'
 ], function(
   domReady
+  ,pathData
   ,core
 ) {
   
@@ -20,10 +22,42 @@ require([
   var parser = new DOMParser;
   
   renderbutton.onclick = function() {
-    var svgdoc = JSON.parse(svgtext.value);
-    svgdoc.paths.forEach(function(path) {
-      console.log(path);
-    });
+    var doc = parser.parseFromString(svgtext.value, 'application/xml');
+    rendercanvas.width = doc.documentElement.getAttribute('width') || 300;
+    rendercanvas.height = doc.documentElement.getAttribute('height') || 150;
+    var ctx = rendercanvas.getContext('2d');
+    ctx.clearRect(0, 0, rendercanvas.width, rendercanvas.height);
+    for (var node = doc.documentElement.firstChild; node; node = node.nextSibling) {
+      if (node.nodeType !== 1) continue;
+      switch (node.nodeName) {
+        case 'shape':
+          var path = pathData.parse(node.getAttribute('path') || '');
+          path = pathData.absolutize(path);
+          path = pathData.reduce(path);
+          var points = [];
+          var loop = false;
+          for (var i = 0; i < path.length; i++) {
+            if (path[i].type === 'Z') {
+              loop = true;
+            }
+            else {
+              points.push({x:path[i].values[0], y:path[i].values[1]});
+            }
+          }
+          if (node.hasAttribute('fill')) {
+            ctx.fillStyle = node.getAttribute('fill');
+            core.fill(ctx, points);
+          }
+          if (node.hasAttribute('edges')) {
+            ctx.fillStyle = node.getAttribute('edges');
+            core.lines(ctx, points, loop);
+          }
+          break;
+        default:
+          console.warn('pikvek: unknown element ' + node.nodeName);
+          break;
+      }
+    }
   };
   renderbutton.disabled = false;
   
