e75008f4a90d3b21160614640a4f2205b2d82358
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-03-30 11:55:11 +0100

    Update pikvek-main.js

--- pikvek-main.js
+++ pikvek-main.js
@@ -64,24 +64,4 @@ require([
   };
   renderbutton.disabled = false;
   
-  var points = [
-    {x:247, y:887 - 650},
-    {x:213, y:977 - 650},
-    {x:168, y:893 - 650},
-    {x:73, y:908 - 650},
-    {x:123, y:826 - 650},
-    {x:62, y:752 - 650},
-    {x:158, y:755 - 650},
-    {x:192, y:665 - 650},
-    {x:238, y:749 - 650},
-    {x:333, y:734 - 650},
-    {x:282, y:816 - 650},
-    {x:343, y:890 - 650},
-  ];
-  var ctx = rendercanvas.getContext('2d');
-  ctx.fillStyle = '#fff';
-  core.fill(ctx, points);
-  ctx.fillStyle = '#000';
-  core.lines(ctx, points, true);
-  
 });
