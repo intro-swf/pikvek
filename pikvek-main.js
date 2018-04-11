39d394ca25237365e589478b3dc6c421ca3e319e
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-04-11 22:58:52 +0100

    Update pikvek-main.js

--- pikvek-main.js
+++ pikvek-main.js
@@ -137,10 +137,10 @@ require([
           var oppositeMidY2 = oppositeMidY + (vanish2.y - vanish1.y);
           var opposite1 = getCrossingPoint(vanish1.x, vanish1.y, ax, ay, oppositeMidX,oppositeMidY, oppositeMidX2,oppositeMidY2);
           var opposite2 = getCrossingPoint(vanish2.x, vanish2.y, cx, cy, oppositeMidX,oppositeMidY, oppositeMidX2,oppositeMidY2);
-          const xBaseX = opposite1.x, xBaseY = opposite1.y;
+          const xBaseX = opposite2.x, xBaseY = opposite2.y;
           const xOffX = (oppositeMidX - xBaseX) / srcBox.width, xOffY = (oppositeMidY - xBaseY) / srcBox.height;
           const yBaseX = oppositeMidX, yBaseY = oppositeMidY;
-          const yOffX = (opposite2.x - yBaseX) / srcBox.width, yOffY = (opposite2.y - yBaseY) / srcBox.height;
+          const yOffX = (opposite1.x - yBaseX) / srcBox.width, yOffY = (opposite1.y - yBaseY) / srcBox.height;
           console.log(xBaseX,xBaseY, xOffX,xOffY, yBaseX,yBaseY, yOffX,yOffY);
           function transform(point) {
             return getCrossingPoint(
