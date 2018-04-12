8c81aacaf604d589614aa834c62b5cddd67c6751
intro-swf <33427068+intro-swf@users.noreply.github.com>
2018-04-12 10:41:42 +0100

    Update pikvek-main.js

--- pikvek-main.js
+++ pikvek-main.js
@@ -158,6 +158,13 @@ require([
           }
           window.transformPoint = transform;
           window.transformPointReverse = reverseTransform;
+          ctx.fillStyle = '#000';
+          for (var i = 0; i <= 10; i++) {
+            var xi = srcBox.x + i*srcBox.width/10;
+            var yi = srcBox.y + i*srcBox.height/10;
+            core.lines(ctx, [transform(xi, srcBox.y), transform(xi, srcBox.y + srcBox.height)], false);
+            core.lines(ctx, [transform(srcBox.x, yi), transform(srcBox.x + srcBox.width, yi)], false);
+          }
           break;
         default:
           console.warn('pikvek: unknown element ' + node.nodeName);
